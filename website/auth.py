2890b178e5a917db2275e0bc740940850b53860c
Benkivuva <benkivuvak5@gmail.com>
2023-09-02 10:34:53 +0300

    handled post requests

--- website/auth.py
+++ website/auth.py
@@ -7,13 +7,13 @@ login, logout, and sign-up.
 
 """
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 # Create the 'auth' blueprint
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     """
     Display the login page.
@@ -29,9 +29,26 @@ def logout():
     return "<p>Log Out</p>"
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     """
     Display the sign-up page.
     """
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            pass
+        elif len(firstName) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif password1 < 7:
+            pass
+        else:
+            # add user to db
+            pass
     return render_template("signup.html")
