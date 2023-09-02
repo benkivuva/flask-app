d0f633cb10a1e38fdf7127e7f712675517efed5c
Benkivuva <benkivuvak5@gmail.com>
2023-09-02 11:15:42 +0300

    setup SQLAlchemy

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+# Create a SQLAlchemy database instance
+db = SQLAlchemy()
+
+# Define the database file name
+DB_NAME = "database.db"
 
 def create_app():
     """
@@ -14,7 +20,13 @@ def create_app():
     # Set the SECRET_KEY for session management
     app.config['SECRET_KEY'] = 'mimikamaishuuuuu'
 
-    # Import blueprints
+    # Configure the SQLAlchemy database URI
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    # Initialize the SQLAlchemy extension with the app
+    db.init_app(app)
+
+    # Import blueprints for modularization
     from .views import views
     from .auth import auth
 
